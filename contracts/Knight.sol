d77cdf7390b2e058d17e55a8b740c16a1ae5f41f
Quincy <quincy.chen@grabtaxi.com>
2018-07-04 18:44:03 +0800

    update solidity

--- contracts/Knight.sol
+++ contracts/Knight.sol
@@ -4,12 +4,12 @@ contract CryptoKnight {
 
   event NewKnight(uint knightId, string name, uint dna);
 
-  uint dnaDigits = 8;
+  uint8 dnaDigits = 8;
   uint dnaModulus = 10 ** dnaDigits;
 
   struct Knight {
     string name;
-    uint dna;
+    uint8 dna;
   }
 
   Knight[] public knights;
@@ -17,28 +17,29 @@ contract CryptoKnight {
   mapping (uint => address) public knightToOwner;
   mapping (address => uint) public ownerKnightCount;
 
-  function _createKnight(string _name, uint _dna) private {
-    uint id = knights.push(Knight(_name, _dna)) - 1;
+  function _createKnight(string _name, uint8 _dna) private {
+    Knight memory knight = Knight(_name, _dna);
+    uint id = knights.push(knight) - 1;
     knightToOwner[id] = msg.sender;
     ownerKnightCount[msg.sender]++;
     NewKnight(id, _name, _dna);
   }
 
-  function _generateRandomDna(string _str) private view returns (uint) {
+  function _generateRandomDna(string _str) private view returns (uint8) {
     uint rand = uint(keccak256(_str));
-    return rand % dnaModulus;
+    return uint8(rand % dnaModulus);
   }
 
   function createRandomKnight(string _name) public {
-    uint randDna = _generateRandomDna(_name);
+    uint8 randDna = _generateRandomDna(_name);
     _createKnight(_name, randDna);
   }
 
-  function getKnightsCount() public constant returns (uint) {
+  function getKnightsCount() public view returns (uint) {
     return knights.length;
   }
 
-  function getKnight(uint index) public constant returns(string, uint) {
+  function getKnight(uint index) public view returns(string, uint) {
     return (knights[index].name, knights[index].dna);
   }
 }
